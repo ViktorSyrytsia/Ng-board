3d415dbacf6a069cc5af959969920ed77d3a5032
ViktorSyrytsia <viktor.syrytsia@jetsoftpro.com>
2021-01-30 23:48:51 +0200

    added firebase module

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBiMmRVhfkME_LBBomvAZB3KUtvihduqNU',
+    authDomain: 'ngboard-59b83.firebaseapp.com',
+    projectId: 'ngboard-59b83',
+    storageBucket: 'ngboard-59b83.appspot.com',
+    messagingSenderId: '201689956817',
+    appId: '1:201689956817:web:a03ea996b1d683abb460f0',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBiMmRVhfkME_LBBomvAZB3KUtvihduqNU',
+    authDomain: 'ngboard-59b83.firebaseapp.com',
+    projectId: 'ngboard-59b83',
+    storageBucket: 'ngboard-59b83.appspot.com',
+    messagingSenderId: '201689956817',
+    appId: '1:201689956817:web:a03ea996b1d683abb460f0',
+  },
 };
 
 /*
